a54195722e8dc2ff95db5c53a9bd59e1d9ed3d57
Brijoy <brijoydmello@gmail.com>
2024-08-20 23:15:32 +0530

    actuator added Commit

--- src/main/java/org/genbank/LoanAPI/configuration/AppSecurityConfig.java
+++ src/main/java/org/genbank/LoanAPI/configuration/AppSecurityConfig.java
@@ -1,7 +1,6 @@
 package org.genbank.LoanAPI.configuration;
 
 import org.genbank.LoanAPI.filter.JwtFilter;
-import org.genbank.LoanAPI.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +9,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
